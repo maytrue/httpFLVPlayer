896b0464098d7214edd9a11d339ef1604c60ce91
guowei <guowzou@gmail.com>
2016-01-14 11:56:08 +0800

    init project

---
+++ ios/httpFLVPlayer/httpFLVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  httpFLVPlayer
+//
+//  Created by guowei on 1/14/16.
+//  Copyright © 2016 guowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
